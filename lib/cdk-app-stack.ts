2b41cee201ff60ed8c830c4da71989a3e3bc8c01
Luxurious <luxurious@PRO-X64-D6M.local>
2019-12-30 18:00:37 +1100

    Initial Commit

--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -1,9 +1,16 @@
-import cdk = require('@aws-cdk/core');
+import {App, Stack, StackProps} from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
+import kms = require('@aws-cdk/aws-kms');
 
-export class CdkAppStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class CdkAppStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
-
+    const cmk = kms.Key.fromKeyArn(this, 'cmk', 'replace with your customer managed CMK ARN')
+    new s3.Bucket(this, 'cdkBucket', {
+      versioned: true,
+      encryption: s3.BucketEncryption.KMS,
+      encryptionKey: cmk
     // The code that defines your stack goes here
-  }
+  });
+ }
 }
